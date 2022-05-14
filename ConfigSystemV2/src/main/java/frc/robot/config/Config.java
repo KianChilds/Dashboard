28c0fae55c3784a02f2440c6c163224285378582
HowdyBots <94756266+KianChilds@users.noreply.github.com>
2022-05-14 14:06:42 -0500

    Got more config working

--- ConfigSystemV2/src/main/java/frc/robot/config/Config.java
+++ ConfigSystemV2/src/main/java/frc/robot/config/Config.java
@@ -2,10 +2,12 @@ package frc.robot.config;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
+import frc.robot.subsystems.driveTrain.DriveTrainConfig;
+
 public class Config {
-    public int version;
+    public int version = 2;
     @JsonIgnore
     public RuntimeConfig runtimeConfig;
-
     
+    public DriveTrainConfig driveTrainConfig = new DriveTrainConfig();
 }
